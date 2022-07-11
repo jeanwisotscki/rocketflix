c157f28a49f0b7d8dfd316fc5db049c796ea1f87
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-07-11 18:09:16 -0300

    att api

---
+++ api.js
@@ -0,0 +1,4 @@
+export const API_KEY = "api_key=4538940946a76b0d8a1986cb89f0cc4b";
+export const BASE_URL = "https://api.themoviedb.org/3/movie/";
+export const IMG_URL = "https://image.tmdb.org/t/p/w500";
+export const language = "language=pt-BR";
